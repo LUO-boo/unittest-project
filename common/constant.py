5a8b30eb10a5c7941bbda1ad8c4cd33b9a82b50f
test <1473709778@qq.com>
2022-01-01 16:21:05 +0800

    接口自动化

---
+++ common/constant.py
@@ -0,0 +1,5 @@
+"""
+driver_status:司机锁定的状态
+"""
+#存接口返回的数据
+con={"driver_status":None}
---
+++ common/get_conf.py
@@ -0,0 +1,13 @@
+import os
+from configparser import ConfigParser
+from common.set_path import conf_path
+class Conf(ConfigParser):
+    def __init__(self,conf_name):
+        """
+        重写调用配置文件的方法
+        :param conf_name:
+        """
+        super().__init__()
+        self.read(conf_name,encoding="utf-8")
+
+conf=Conf(os.path.join(conf_path,"conf.ini"))
